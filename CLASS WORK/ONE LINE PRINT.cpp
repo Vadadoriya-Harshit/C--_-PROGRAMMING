747ac8f3687d863b2ddfbbf69304b54552b12ed9
Harshuv9 <harshitpatelv9@gmail.com>
2022-12-28 20:08:42 -0800

    Create ONE LINE PRINT.cpp

---
+++ CLASS WORK/ONE LINE PRINT.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	cout<<"welcome to c++ programming"<<endl;
+	cout<<"in tops technology"<<endl;
+	int a=43, b=64;
+	
+	cout<<"A = "<<a<<"\nB ="<<b;
+	
+	
+	return 0;
+}
