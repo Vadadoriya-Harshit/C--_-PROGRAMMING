843c5d9c5622be7fbb8c2da6163d67127370ea78
Harshuv9 <harshitpatelv9@gmail.com>
2023-01-23 00:54:25 +0530

    Update 15.cpp

--- ASSIGNMENTS/15.cpp
+++ ASSIGNMENTS/15.cpp
@@ -2,39 +2,52 @@
 using namespace std;
 class addition{
 	public:
-		int arr1[5];
+		int arr1[5],arr2[5];
 		int i;
 		int sum;
 		void add()
 		{
-			cout<<"\n\n\t\tEnter 1D Array Elements Are : ";
+			cout<<"\n\n\t\tEnter 1D Array 1 Elements Are : ";
 			for (int i=0;i<5;++i)
 			{
 				cout<<"\n\n\t\t tEnter Element["<<i<<"] :- ";
 				cin>>arr1[i];
 			}
+			cout<<"\n\n\t\tEnter 1D Array 2 Elements Are : ";
+			for (int i=0;i<5;++i)
+			{
+				cout<<"\n\n\t\t tEnter Element["<<i<<"] :- ";
+				cin>>arr2[i];
+			}
+			
 			
 		}
 		void display()
 		{
-			cout<<"\n\n\t\tEnter 1D Array Elements Are : ";
+			cout<<"\n\n\t\tEnter 1D Array 1 Elements Are : ";
 			for (int i=0;i<5;++i)
 			{
 				cout<<"\n\n\t\t tEnter Element["<<i<<"] :- "<<arr1[i];
 				
 			}
-		}
-		void result()
-		{
-			for(i=0;i<5;i++)
+			cout<<"\n\n\t\tEnter 1D Array 2 Elements Are : ";
+			for (int i=0;i<5;++i)
 			{
-			sum+=arr1[i];
-		    }
-		cout<<"\n\n\t\t Addition Of 1D Array Is :- "<<sum;
+				cout<<"\n\n\t\t tEnter Element["<<i<<"] :- "<<arr2[i];
 				
+			}
+		}
+//		void result()
+//		{
+//			for(i=0;i<5;i++)
+//			{
+//			
+//		    }
+//		cout<<"\n\n\t\t Addition Of 1D Array Is :- "<<sum;
+//				
 				
 			
-		}
+		//}
 		
 };
 int main()
@@ -42,6 +55,6 @@ int main()
 	addition ad;
 	ad.add();
 	ad.display();
-	ad.result();
+	//ad.result();
 	return 0;
 }
